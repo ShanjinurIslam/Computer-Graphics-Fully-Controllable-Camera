03bbcf5bf9284476917d6fa2a96837aa3b416e4f
ShanjinurIslam <spondoncsebuet@gmail.com>
2019-05-23 23:17:08 +0600

    Initial Commit

---
+++ Offline1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Offline1
+//
+//  Created by Shanjinur Islam on 5/23/19.
+//  Copyright © 2019 Shanjinur Islam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
