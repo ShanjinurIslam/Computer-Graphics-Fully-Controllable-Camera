4432176d89ea53055783c4babf74fae84766d126
ShanjinurIslam <spondoncsebuet@gmail.com>
2019-05-23 23:31:12 +0600

    Initial Setup

--- Offline1/main.cpp
+++ Offline1/main.cpp
@@ -5,11 +5,43 @@
 //  Created by Shanjinur Islam on 5/23/19.
 //  Copyright © 2019 Shanjinur Islam. All rights reserved.
 //
-
+#define GL_SILENCE_DEPRECATION
 #include <iostream>
+#include <GLUT/GLUT.h>
+#include <OpenGL/glu.h>
+#include <OpenGL/gl.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void init(){
+    
+}
+void display(){
+    
+}
+void animate(){
+    
+}
+void keyboardListener(unsigned char key,int x,int y){
+    
+}
+void specialKeyListener(int key,int x,int y){
+    
+}
+
+
+int main(int argc, char **argv)
+{
+    glutInit(&argc,argv);
+    glutInitWindowSize(500, 500);
+    glutInitWindowPosition(0, 0);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
+    glutCreateWindow("Offline 1");
+    init();
+    glEnable(GL_DEPTH_TEST);
+    glutDisplayFunc(display);
+    glutIdleFunc(animate);
+    glutKeyboardFunc(keyboardListener);
+    glutSpecialFunc(specialKeyListener);
+    glutMainLoop();
+    
     return 0;
 }
